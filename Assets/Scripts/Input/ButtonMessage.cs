02cb83f83124187c2f0c1fc880eed9b9675c28e2
sarmenufam <sarmenufam@gmail.com>
2014-01-14 18:18:54 -0400

    Criação da Base do Jogo

---
+++ Assets/Scripts/Input/ButtonMessage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonMessage : MonoBehaviour {
+
+	public string nameMessageMethod;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnClick () {
+		target.SendMessage(nameMessageMethod,SendMessageOptions.DontRequireReceiver);
+	}
+
+	void OnMouseOver () {
+
+				
+	}
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoToSelectLevel(){
+
+	}
+	public void exit(){
+		Application.Quit();
+	}
+	public void PlayGame(){
+		Application.LoadLevel("GamePlay");
+	}
+}
